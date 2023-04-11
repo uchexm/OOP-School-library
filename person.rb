11dd3f9074014833e5f9a867d397d14d80fd0504
uchexm <109084069+uchexm@users.noreply.github.com>
2023-04-11 11:55:35 +0200

    Create a nameable file

--- person.rb
+++ person.rb
@@ -1,21 +1,28 @@
-class Person
-  attr_reader :id
-  attr_accessor :name, :age
+require_relative 'nameable'
 
-  def initialize(age, name = 'unknown', parent_permission: true)
-    @id = Random.rand(1..100)
+class Person < Nameable
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  private
+
   def of_age?
     @age >= 18
   end
-
-  def can_use_services?
-    @age >= 18 || parent_permission
-  end
-
-  private :is_of_age?
 end
