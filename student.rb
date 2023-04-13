71e53d3258e2238cd6d74d6101afb56c954c0910
uchexm <109084069+uchexm@users.noreply.github.com>
2023-04-13 10:41:21 +0200

    update student class

--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
   def play_hooky
-    '¯(ツ)/¯'
+    "¯\(ツ)/¯"
   end
 end
