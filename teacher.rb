8feb10ac9cb2be0bb9ff7fc08ba808c5a3f9db64
uchexm <109084069+uchexm@users.noreply.github.com>
2023-04-13 16:46:18 +0200

    Update bug

--- teacher.rb
+++ teacher.rb
@@ -4,8 +4,6 @@ class Teacher < Person
   attr_accessor :specialization
 
   def initialize(specialization, age, name = 'Unknown', id = Random.rand(1..1000))
-    binding.pry
-    #I switched the specialization with age
     super(age, name, id)
     @specialization = specialization
   end
