0a3c44764ff1898239e687ea12f98fe3e58cdebd
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-04-17 18:20:41 +0500

    Refactored using SOLID principles

--- main.rb
+++ main.rb
@@ -1,55 +1,49 @@
-require_relative 'app'
+require_relative './app'
 
-def ask_user
-  puts 'Select what you want to do'
-  puts "
-    1 List all books.
-    2 List all people.
-    3 Create a person.
-    4 Create a book.
-    5 Create a rental.
-    6 List all rentals for a given person id.
-    7 Exit."
-  gets.chomp.to_i
-end
+# Ask the user for the choice
+class Main
+  def menu
+    @list = {
+      '1' => 'List all books',
+      '2' => 'List all people',
+      '3' => 'Create a person',
+      '4' => 'Create a book',
+      '5' => 'Create a rental',
+      '6' => 'List all rentals for a given person id',
+      '7' => 'Exit'
+    }
 
-def loop_books(my_app)
-  my_app.books.each_with_index do |book, index|
-    puts "#{index + 1}) Title: #{book.title}, Author: #{book.author}"
+    @list.each do |index, string|
+      puts "#{index} - #{string}"
+    end
+    gets.chomp.to_i
   end
 end
 
-def loop_people(my_app)
-  my_app.people.each_with_index do |person, index|
-    type = person.is_a?(Student) ? 'Student' : 'Teacher'
-    puts "#{index}) [#{type}] Name: #{person.name} ID: #{person.id} Age: #{person.age}"
-  end
-end
+# handle the choices
+def choice(user_answer, response)
+  actions = {
+    1 => :list_books,
+    2 => :list_people,
+    3 => :create_person,
+    4 => :create_book,
+    5 => :create_rental,
+    6 => :list_rentals,
+    7 => :exit_app
+  }
 
-def process_choice(choice, my_app)
-  case choice
-  when 1
-    loop_books(my_app)
-  when 2
-    loop_people(my_app)
-  when 3
-    my_app.create_person
-  when 4
-    my_app.create_book
-  when 5
-    my_app.create_rental
-  when 6
-    puts my_app.view_rental
-  end
+  action = actions[user_answer]
+
+  response.send(action)
 end
 
 def main
-  my_app = App.new
-  loop do
-    choice = ask_user
-    break if choice == 7
+  response = App.new
 
-    process_choice(choice, my_app)
+  loop do
+    puts 'Please choose an option by entering a number'
+    user_answer = Main.new.menu
+    choice user_answer, response
   end
 end
 
